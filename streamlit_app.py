d9962fdb2245701a160bc271173df6920f7f6742
Mohammed Husamuddin <uni.husam@gmail.com>
2023-09-27 22:31:48 +0400

    innit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="CIIED Resources Page",page_icon="CIIED_Logo.png")
-
+st.set_page_config(page_title="CIIED Resources Page", page_icon="CIIED_Logo.png")
 
 # Define the title and description for your app
 st.title("ACM X CIIED Student Hackathon")
@@ -15,7 +14,7 @@ if st.button("Access Pitching Presentation Template"):
 st.subheader("Hackathon Details")
 st.markdown("This student hackathon is organized by ACM and CIIED Dubai. Here are some key details:")
 st.markdown("- Date: 28th September")
-st.markdown("- Time: From 3PM")
+st.markdown("- Time: From 3 PM")
 st.markdown("- Duration: 2 mins pitch + 5 mins Q&A")
 st.markdown("- Location: Auditorium")
 st.markdown("- Registration Deadline: 27th September")
@@ -38,7 +37,6 @@ with st.sidebar.expander("How to Design a Better Pitch Deck", expanded=True):
         * **Practice, practice, practice.** Before you deliver your pitch, practice it multiple times in front of a mirror or with a friend. This will help you to deliver your presentation smoothly and confidently.
     """)
 
-
 # Add rules for the hackathon
 st.subheader("Hackathon Rules")
 st.markdown("To ensure a fair and productive hackathon experience, please adhere to the following rules:")
@@ -53,12 +51,48 @@ st.markdown("7. The decision of the judges is final.")
 # Add prizes and awards information
 st.subheader("Prizes and Awards")
 st.markdown("We have exciting prizes and awards for the winners:")
-st.markdown("- 1st Place: [Prize Details]")
-st.markdown("- 2nd Place: [Prize Details]")
-st.markdown("- 3rd Place: [Prize Details]")
+st.markdown("- 1st Place: 200 AED")
+st.markdown("- 2nd Place: 150 AED")
+st.markdown("- 3rd Place: 100 AED")
+st.markdown("- Best Pitch: 50 AED")
+st.markdown("- Best Prototype: 50 AED")
+st.markdown("Winners will get an exclusive chance to access the BPDC Incubation centre!")
 
-# You can add more sections, resources, or information as needed
+# Add participation criteria
+st.subheader("Criteria for Participation")
+st.markdown("This hackathon is open to all BPDC Students.")
+st.markdown("Team size should be between 2 and 4 members.")
+st.markdown("Participation is free of charge.")
+st.markdown("No prior coding experience is necessary for participation.")
+st.markdown("ACM and CIIED will issue certificates for all participants!")
+
+# Add a timeline
+st.subheader("Timeline")
+st.markdown("(All times are in GST)")
+
+# Create a timeline slider
+timeline = {
+    "27th September": {
+        "7th Hour (1-2 PM)": "Introduction to the Hackathon (Room G34)",
+        "2:00 PM": "Hackathon Themes Are Released (Online)",
+    },
+    "28th September": {
+        "9:00 AM - 3:00 PM": "Registration and Brainstorming Session (Auditorium)",
+        "9th Hour (3 - 4 PM)": "Pitching - Workshop (Auditorium)",
+        "4:00 PM - 5:30 PM": "Brainstorming Session (Auditorium)",
+        "5:30 PM - 6:00 PM": "Concluding Speeches (Auditorium)",
+        "6:00 PM - 7:00 PM": "Pitching Round to Judges (Room G34)",
+        "7:00 PM - 7:15 PM": "Buses leave for Day Scholars (Outside Minimart)",
+    },
+}
+
+selected_date = st.selectbox("Select a Date", list(timeline.keys()))
+selected_events = timeline[selected_date]
+
+st.markdown(f"**{selected_date}**")
+for time, event in selected_events.items():
+    st.markdown(f"- **{time}**: {event}")
 
 # Footer
 st.markdown("For any questions or assistance, please contact us at ciie@dubai.bits-pilani.ac.in.")
-
+st.markdown("Made with ❤️ by ACM and CIIED")
